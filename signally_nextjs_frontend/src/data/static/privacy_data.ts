b2f8a6a6dd458c17c8f6f311fdcda58239aa7dc4
godofmarket <126969028+godofmarket@users.noreply.github.com>
2023-03-04 16:22:41 +0100

    Add files via upload

---
+++ signally_nextjs_frontend/src/data/static/privacy_data.ts
@@ -0,0 +1,61 @@
+export const privacyPolicy = [
+  {
+    id: '1',
+    title: 'Effective as of October 25, 2021',
+    description: `<p>Protecting your private information is our priority. This Statement of Privacy applies to www.signally.com and Signally and governs data collection and usage. For the purposes of this Privacy Policy, unless otherwise noted, all references to Signally include www.signally.com. The Signally website is a Financial Technology site. By using the Signally website, you consent to the data practices described in this statement</p>
+      `
+  },
+  {
+    id: '2',
+    title: 'Collection of your Personal Information',
+    description: `<p>In order to better provide you with products and services offered on our Site, Signally may collect personally identifiable information, such as your:</p>
+    <ol>
+    <li>First and Last Name</li>
+    <li>Mailing Address</li>
+    <li>E-mail Address</li>
+    <li>Date of Birth</li>
+    <li>Country of Citizenship</li>
+    </ol>
+    <p>If you purchase Signally products and services, we collect billing and credit card information through our trusted payment processors. This information is used to complete the purchase transaction. We do not collect any personal information about you unless you voluntarily provide it to us. However, you may be required to provide certain personal information to us when you elect to use certain products or services available on the Site. These may include: (a) registering for an account on our Site; (b) entering a sweepstakes or contest sponsored by us or one of our partners; (c) signing up for special offers from selected third parties; (d) sending us an email message; (e) submitting your credit card or other payment information when ordering and purchasing products and services on our Site. To wit, we will use your information for, but not limited to, communicating with you in relation to services and/or products you have requested from us. We also may gather additional personal or non-personal information in the future.</p>
+    `
+  },
+  {
+    id: '3',
+    title: 'Use of your Personal Information',
+    description: `<p>Signally collects and uses your personal information to operate its website(s) and deliver the services you have requested.
+    Signally may also use your personally identifiable information to inform you of other products or services available from Signally and its affiliates.
+    Sharing Information with Third Parties
+    Signally does not sell, rent or lease its customer lists to third parties.
+    Signally may share data with trusted partners to help perform statistical analysis, send you email or postal mail, provide customer support, or arrange for deliveries. All such third parties are prohibited from using your personal information except to provide these services to Signally, and they are required to maintain the confidentiality of your information.
+    Signally may disclose your personal information, without notice, if required to do so by law or in the good faith belief that such action is necessary to: (a) conform to the edicts of the law or comply with legal process served on Signally or the site; (b) protect and defend the rights or property of Signally; and/or (c) act under exigent circumstances to protect the personal safety of users of Signally, or the public.
+    </p>
+    `
+  },
+  {
+    id: '4',
+    title: 'Tracking User Behavior',
+    description: `<p>Signally may keep track of the websites and pages our users visit within Signally, in order to determine what Signally services are the most popular. This data is used to deliver customized content and advertising within Signally to customers whose behavior indicates that they are interested in a particular subject area.</p>
+    `
+  },
+  {
+    id: '5',
+    title: 'Automatically Collected Information',
+    description: `<p>Information about your computer hardware and software may be automatically collected by Signally. This information can include: your IP address, browser type, domain names, access times and referring website addresses. This information is used for the operation of the service, to maintain quality of the service, and to provide general statistics regarding use of the Signally website.</p>
+      `
+  },
+  {
+    id: '6',
+    title: 'Address Book',
+    description: `<p>Signally uses emails as unique identifiers so that it is easy for you to switch from one device to another and retain your social graph.</p>
+    `
+  },
+  {
+    id: '7',
+    title: 'Use of Cookies',
+    description: `<p>The Signally website may use "cookies" to help you personalize your online experience. A cookie is a text file that is placed on your hard disk by a web page server. Cookies cannot be used to run programs or deliver viruses to your computer. Cookies are uniquely assigned to you, and can only be read by a web server in the domain that issued the cookie to you.
+    One of the primary purposes of cookies is to provide a convenience feature to save you time. The purpose of a cookie is to tell the Web server that you have returned to a specific page. For example, if you personalize Signally pages, or register with Signally site or services, a cookie helps Signally to recall your specific information on subsequent visits. This simplifies the process of recording your personal information, such as billing addresses and so on. When you return to the same Signally website, the information you previously provided can be retrieved, so you can easily use the Signally features that you customized.
+    You have the ability to accept or decline cookies. Most Web browsers automatically accept cookies, but you can usually modify your browser setting to decline cookies if you prefer. If you choose to decline cookies, you may not be able to fully experience the interactive features of the Signally services or websites you visit.
+    </p>
+    `
+  }
+];
